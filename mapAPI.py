c2461592e98488b417dd462b9af46647eef500b1
AP020897 <aphyo@mail.sfsu.edu>
2017-11-16 11:16:34 -0800

    user search with address and show the geolocation

--- mapAPI.py
+++ mapAPI.py
@@ -1,10 +1,9 @@
 import sys
 import urllib.request
 from PyQt5 import QtNetwork, QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
 from PyQt5.QtGui import QIcon, QPixmap
 from geolocation.main import GoogleMaps
-import gmplot
 
 class App(QWidget):
 
@@ -13,9 +12,10 @@ class App(QWidget):
         self.title = 'PyQt5 image - pythonspot.com'
         self.left = 10
         self.top = 10
-        self.width = 640
-        self.height = 480
+        self.width = 800
+        self.height = 600
         self.initUI()
+        self.address = ""
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -26,38 +26,41 @@ class App(QWidget):
         self.nam = QtNetwork.QNetworkAccessManager()
         self.nam.finished.connect(self.handleResponse)
 
-        address = "6171 Mission st, San Francisco"
+        #textbox
+        self.newaddress = QLineEdit(self)
+        self.newaddress.move(20,440)
+        self.newaddress.resize(200,40)
+
+        #location button
+        self.button = QPushButton('location', self)
+        self.button.setStyleSheet("background-color: grey")
+        self.button.move(20,500)
+
+        self.button.clicked.connect(self.geolocation)
+
+        self.address = "6171 Mission st, San Francisco"
 
         google_maps = GoogleMaps(api_key='AIzaSyDlJqxwlOWWAPwf54ivrpAZw4R1Yb5j6Yk')
 
-        location = google_maps.search(location=address) # sends search to Google Maps.
+        location = google_maps.search(location=self.address) # sends search to Google Maps.
 
         my_location = location.first() # returns only first location.
 
         # get zoom level
         #z = sys.argv[1]
         mapUrl = 'http://maps.googleapis.com/maps/api/staticmap'
-        mapUrl = mapUrl + '?size=400x400'
+        mapUrl = mapUrl + '?size=800x400'
         #mapUrl = mapUrl + '&center=37.721,-122.4765'
         mapUrl = mapUrl + '&center=' + str(my_location.lat) + ',' + str(my_location.lng)
         #mapUrl = mapUrl + '&center=1600+Holloway+Ave,San+Francisco,CA'
         mapUrl = mapUrl + '&zoom=' + str(15)
         req = QtNetwork.QNetworkRequest(QtCore.QUrl(mapUrl))
 
-        #self.gmap = gmplot.GoogleMapPlotter(my_location.lat, my_location.lng)
-
-        #self.gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
-        #self.gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-        #self.gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-        #self.gmap.heatmap(heat_lats, heat_lngs)
-
-        #self.gmap.draw("mymap.html")
 
         self.nam.get(req)
 
         self.pixmap = QPixmap()
         self.show()
-
 
     def handleResponse(self, reply):
 
@@ -69,6 +72,34 @@ class App(QWidget):
             self.label.setPixmap(self.pixmap)
             self.label.resize(self.pixmap.width(), self.pixmap.height())
             self.label.show()
+
+    def geolocation(self):
+
+        google_maps = GoogleMaps(api_key='AIzaSyDlJqxwlOWWAPwf54ivrpAZw4R1Yb5j6Yk')
+
+        location = google_maps.search(location=self.newaddress.text()) # sends search to Google Maps.
+
+        my_location = location.first() # returns only first location.
+
+        mapUrl = 'http://maps.googleapis.com/maps/api/staticmap'
+        mapUrl = mapUrl + '?size=800x400'
+        mapUrl = mapUrl + '&center=' + str(my_location.lat) + ',' + str(my_location.lng)
+        mapUrl = mapUrl + '&zoom=' + str(15)
+        #&markers=color:blue%7Clabel:S%7C40.702147,-74.015794
+        mapUrl = mapUrl + '&markers=color:blue'
+        mapUrl = mapUrl + '%' + str(7) + 'Clabel:S%' + str(7) + 'C'
+        mapUrl = mapUrl + str(my_location.lat) + ',' + str(my_location.lng)
+        mapUrl = mapUrl + '&sensor=true'
+        #mapUrl = mapUrl + '&markers=color:blue|label:S|' + str(my_location.lat) + ',' + str(my_location.lng)
+        req = QtNetwork.QNetworkRequest(QtCore.QUrl(mapUrl))
+
+        self.nam.get(req)
+
+        self.pixmap = QPixmap()
+        self.show()
+
+        print('lat is ' + str(my_location.lat))
+        print('lng is ' + str(my_location.lng))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
